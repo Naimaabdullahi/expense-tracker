c105c4f2a0315631d0b4c4b16b2c2388b33b37d3
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-04-13 23:16:56 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Vite configuration for React project
 export default defineConfig({
   plugins: [react()],
-  base: "/expense-tracker/",
+  base: "/expense-tracker/"
 });
